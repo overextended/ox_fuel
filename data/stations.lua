3e14549d0250e97c0661a0e1cfd3c0b79cdfba8b
Luke <sabolukas03@gmail.com>
2022-01-15 16:09:41 +0100

    feat: Los Santos fuel station zones

---
+++ data/stations.lua
@@ -0,0 +1,110 @@
+-- Los Santos stations
+stations = {
+    {
+        name = 'fuel-1',
+        coords = vector3(-71.28, -1761.16, 29.48),
+        heading = 340,
+        length = 13.8,
+        width = 26.2,
+        minZ = 28.48,
+        maxZ = 32.48
+    },
+    {
+        name = 'fuel-2',
+        coords = vector3(264.74, -1260.98, 29.18),
+        length = 19.2,
+        width = 25.8,
+        heading = 0,
+        minZ = 28.18,
+        maxZ = 32.18
+    },
+    {
+        name = 'fuel-3',
+        coords = vector3(1208.66, -1402.64, 35.22),
+        length = 14.2,
+        width = 12.0,
+        heading = 45,
+        minZ = 34.22,
+        maxZ = 38.22
+    },
+    {
+        name = 'fuel-4',
+        coords = vector3(818.83, -1029.89, 26.17),
+        length = 10,
+        width = 25.0,
+        heading = 0,
+        minZ = 25.17,
+        maxZ = 29.17
+    },
+    {
+        name = 'fuel-5',
+        coords = vector3(1181.27, -329.57, 69.18),
+        length = 27.2,
+        width = 11.8,
+        heading = 10,
+        minZ = 68.18,
+        maxZ = 72.18
+    },
+    {
+        name = 'fuel-6',
+        coords = vector3(621.07, 269.52, 103.04),
+        length = 17.0,
+        width = 27.4,
+        heading = 0,
+        minZ = 102.04,
+        maxZ = 106.04
+    },
+    {
+        name = 'fuel-7',
+        coords = vector3(-1437.58, -276.38, 46.21),
+        length = 20.2,
+        width = 20.6,
+        heading = 40,
+        minZ = 45.21,
+        maxZ = 49.21
+    },
+    {
+        name = 'fuel-8',
+        coords = vector3(-2096.6, -318.15, 13.02),
+        length = 21.6,
+        width = 25.8,
+        heading = 354,
+        minZ = 12.02,
+        maxZ = 16.02
+    },
+    {
+        name = 'fuel-9',
+        coords = vector3(-1799.03, 803.11, 138.4),
+        length = 14.8,
+        width = 25.8,
+        heading = 43,
+        minZ = 137.4,
+        maxZ = 141.4
+    },
+    {
+        name = 'fuel-10',
+        coords = vector3(-524.84, -1211.02, 18.18),
+        length = 16.6,
+        width = 19.4,
+        heading = 65,
+        minZ = 17.18,
+        maxZ = 21.18
+    },
+    {
+        name = 'fuel-11',
+        coords = vector3(319.99, -1471.79, 30.51),
+        length = 18.8,
+        width = 27.2,
+        heading = 30,
+        minZ = 29.31,
+        maxZ = 33.51
+    },
+    {
+        name = 'fuel-12',
+        coords = vector3(2581.56, 361.65, 108.46),
+        length = 15.4,
+        width = 25.6,
+        minZ = 107.46,
+        maxZ = 111.46
+    }
+}
