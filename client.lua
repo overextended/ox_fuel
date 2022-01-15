b45eabb8d639c539c1b11f45b64338fa4401f48a
Luke <sabolukas03@gmail.com>
2022-01-15 22:57:58 +0100

    feat: Rough vehicle fueling
    This still needs a lot of work, the way the fueling tick works and the amount of fuel to add especially. Otherwise the general running out of fuel and being able to refuel loop works.

--- client.lua
+++ client.lua
@@ -1,3 +1,37 @@
+local isFueling = false
+local function StartFueling(vehicle)
+    -- todo: refactor, make non ox-inventory
+    isFueling = true
+    local Vehicle = Entity(vehicle).state
+    local fuelAmount = Vehicle.fuel
+    local tick = 200
+    local fuelingDuration = math.ceil((100 - fuelAmount) / 3 * 1000)
+    local fuelToAdd = (100 - fuelAmount) / fuelingDuration * tick -- Need better calculation, not 100% accurate
+    local fuelTick = SetInterval(function()
+        Vehicle:set('fuel', Vehicle.fuel + fuelToAdd)
+    end, tick)
+    exports.ox_inventory:Progress({
+        duration = fuelingDuration,
+        label = 'Fueling vehicle',
+        useWhileDead = false,
+        canCancel = true,
+        disable = {
+            move = true,
+            car = true,
+            combat = true,
+            mouse = false
+        },
+        anim = {
+            dict = 'timetable@gardener@filling_can',
+            clip = 'gar_ig_5_filling_can',
+            flags = 49,
+        },
+    }, function(cancel)
+        ClearInterval(fuelTick)
+        isFueling = false
+    end)
+end
+
 if ox.showBlips == 1 then
 
 elseif ox.showBlips == 2 then
@@ -5,6 +39,15 @@ elseif ox.showBlips == 2 then
 end
 
 local inStation = false
+local gasPumps = {
+    `prop_gas_pump_old2`,
+    `prop_gas_pump_1a`,
+    `prop_vintage_pump`,
+    `prop_gas_pump_old3`,
+    `prop_gas_pump_1c`,
+    `prop_gas_pump_1b`,
+    `prop_gas_pump_1d`,
+}
 
 for i = 1, #ox.stations do
     ox.stations[i]:onPlayerInOut(function(isInside)
@@ -34,3 +77,20 @@ SetInterval(function()
     SetVehicleFuelLevel(vehicle, newFuel)
     Vehicle:set('fuel', newFuel, true)
 end, 1000)
+
+RegisterCommand('startfueling', function()
+    local playerPed = PlayerPedId()
+    if not inStation or GetVehiclePedIsIn(playerPed, false) ~= 0 or isFueling then return end
+    local playerCoords = GetEntityCoords(playerPed)
+    for i = 1, #gasPumps do
+        local pump = GetClosestObjectOfType(playerCoords.x, playerCoords.y, playerCoords.z, 3.0, gasPumps[i], false, false, false)
+        if pump ~= 0 then
+            local vehicle = GetPlayersLastVehicle()
+            if vehicle ~= 0 and #(GetEntityCoords(vehicle) - playerCoords) < 3.0 then
+                TaskTurnPedToFaceEntity(playerPed, vehicle, -1)
+                StartFueling(vehicle)
+            return end
+        break end
+    end
+end)
+RegisterKeyMapping('startfueling', 'Fuel vehicle', 'keyboard', 'e')
