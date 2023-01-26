fcdbe247b818151a62febb0135d3e0933557c992
Linden <65407488+thelindat@users.noreply.github.com>
2023-01-26 13:28:59 +1100

    refactor(client): blips
    Improve visibility with different sprite, colour, and scale.
    Use AddTextEntry for correctness.

--- client.lua
+++ client.lua
@@ -95,16 +95,18 @@ end)
 local isFueling = false
 local nearestPump
 
+AddTextEntry('ox_fuel_station', locale('fuel_station_blip'))
+
 local function createBlip(station)
 	local blip = AddBlipForCoord(station.x, station.y, station.z)
-	SetBlipSprite(blip, 415)
+	SetBlipSprite(blip, 361)
 	SetBlipDisplay(blip, 4)
-	SetBlipScale(blip, 0.6)
-	SetBlipColour(blip, 23)
+	SetBlipScale(blip, 0.8)
+	SetBlipColour(blip, 6)
 	SetBlipAsShortRange(blip, true)
-	BeginTextCommandSetBlipName('STRING')
-	AddTextComponentSubstringPlayerName(locale('fuel_station_blip'))
+	BeginTextCommandSetBlipName('ox_fuel_station')
 	EndTextCommandSetBlipName(blip)
+
 	return blip
 end
 
