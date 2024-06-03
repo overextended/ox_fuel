d6c2d0b3c461083e1741e3d77148a2dd312915e3
Linden <65407488+thelindat@users.noreply.github.com>
2024-06-03 14:32:11 +1000

    refactor(client/stations): use lib.points

--- client/stations.lua
+++ client/stations.lua
@@ -1,81 +1,76 @@
 local config = require 'config'
 local state = require 'client.state'
 local utils = require 'client.utils'
+local stations = lib.load 'data.stations'
 
-CreateThread(function()
-	local stations = lib.load 'data.stations'
+if config.showBlips == 2 then
+	for station in pairs(stations) do utils.createBlip(station) end
+end
 
-	if config.showBlips == 2 then
-		for station in pairs(stations) do utils.createBlip(station) end
+if config.ox_target and config.showBlips ~= 1 then return end
+
+---@param point CPoint
+local function onEnterStation(point)
+	if config.showBlips == 1 and not point.blip then
+		point.blip = utils.createBlip(point.coords)
 	end
+end
 
-	local blip
+---@param point CPoint
+local function nearbyStation(point)
+	if point.currentDistance > 15 then return end
 
-	if config.ox_target and config.showBlips ~= 1 then return end
+	local pumps = point.pumps
+	local pumpDistance
 
-	while true do
-		local playerCoords = GetEntityCoords(cache.ped)
+	for i = 1, #pumps do
+		local pump = pumps[i]
+		pumpDistance = #(cache.coords - pump)
 
-		for station, pumps in pairs(stations) do
-			local stationDistance = #(playerCoords - station)
+		if pumpDistance <= 3 then
+			state.nearestPump = pump
 
-			if stationDistance < 60 then
-				if config.showBlips == 1 and not blip then
-					blip = utils.createBlip(station)
+			repeat
+				local playerCoords = GetEntityCoords(cache.ped)
+				pumpDistance = #(GetEntityCoords(cache.ped) - pump)
+
+				if cache.vehicle then
+					DisplayHelpTextThisFrame('fuelLeaveVehicleText', false)
+				elseif not state.isFueling then
+					local vehicleInRange = state.lastVehicle ~= 0 and
+						#(GetEntityCoords(state.lastVehicle) - playerCoords) <= 3
+
+					if vehicleInRange then
+						DisplayHelpTextThisFrame('fuelHelpText', false)
+					elseif config.petrolCan.enabled then
+						DisplayHelpTextThisFrame('petrolcanHelpText', false)
+					end
 				end
 
-				if not config.ox_target then
-					repeat
-						if stationDistance < 15 then
-							local pumpDistance
+				Wait(0)
+			until pumpDistance > 3
 
-							repeat
-								playerCoords = GetEntityCoords(cache.ped)
-								for i = 1, #pumps do
-									local pump = pumps[i]
-									pumpDistance = #(playerCoords - pump)
+			state.nearestPump = nil
 
-									if pumpDistance < 3 then
-										state.nearestPump = pump
-
-										while pumpDistance < 3 do
-											if cache.vehicle then
-												DisplayHelpTextThisFrame('fuelLeaveVehicleText', false)
-											elseif not state.isFueling then
-												local vehicleInRange = state.lastVehicle ~= 0 and
-													#(GetEntityCoords(state.lastVehicle) - playerCoords) <= 3
-
-												if vehicleInRange then
-													DisplayHelpTextThisFrame('fuelHelpText', false)
-												elseif config.petrolCan.enabled then
-													DisplayHelpTextThisFrame('petrolcanHelpText', false)
-												end
-											end
-
-											pumpDistance = #(GetEntityCoords(cache.ped) - pump)
-											Wait(0)
-										end
-
-										state.nearestPump = nil
-									end
-								end
-								Wait(100)
-							until pumpDistance > 15
-							break
-						end
-
-						Wait(100)
-						stationDistance = #(GetEntityCoords(cache.ped) - station)
-					until stationDistance > 60
-				end
-			end
-		end
-
-		Wait(500)
-
-		if blip then
-			RemoveBlip(blip)
-			blip = nil
+			return
 		end
 	end
-end)
+end
+
+---@param point CPoint
+local function onExitStation(point)
+	if point.blip then
+		point.blip = RemoveBlip(point.blip)
+	end
+end
+
+for station, pumps in pairs(stations) do
+	lib.points.new({
+		coords = station,
+		distance = 60,
+		onEnter = onEnterStation,
+		onExit = onExitStation,
+		nearby = nearbyStation,
+		pumps = pumps,
+	})
+end
