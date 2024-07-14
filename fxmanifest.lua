0e61968983f5aa8b432b60e262572626a0644d9b
Linden <65407488+thelindat@users.noreply.github.com>
2024-07-14 23:01:33 +1000

    chore: update version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ game 'gta5'
 
 name 'ox_fuel'
 author 'Overextended'
-version '1.4.0'
+version '1.5.1'
 repository 'https://github.com/overextended/ox_fuel'
 description 'Fuel management system with ox_inventory support'
 
